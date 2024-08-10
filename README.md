01b6eb87b288fe0d105e6faafd88a783399bc04b
Andrea Del Corto <andrea.delcorto@gmail.com>
2024-08-10 17:31:09 +0200

    first commit :)

---
+++ README.md
@@ -0,0 +1,2 @@
+# Flask tutorial
+https://flask.palletsprojects.com/en/3.0.x/tutorial/
---
+++ flaskr/__init__.py
@@ -0,0 +1,31 @@
+from pathlib import Path
+
+from flask import Flask
+
+
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=Path(app.instance_path) / 'flaskr.sqlite',
+    )
+
+    if test_config is None:
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        app.config.from_mapping(test_config)
+
+    Path(app.instance_path).mkdir(exist_ok=True)
+
+    from . import db
+    db.init_app(app)
+
+    from . import auth
+    app.register_blueprint(auth.bp)
+
+    from . import blog
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
+
+    return app
